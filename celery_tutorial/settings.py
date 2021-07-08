7607dfb04b022d87bef9a7e73a0f8ffaad774328
shubham088 <shubhamsharma0296@gmail.com>
2021-07-09 00:40:44 +0530

    Update settings.py

--- celery_tutorial/settings.py
+++ celery_tutorial/settings.py
@@ -132,6 +132,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shubhamsharma0296@gmail.com'
+EMAIL_HOST_USER = 'host email id'
 EMAIL_HOST_PASSWORD = 'provide password here'
 EMAIL_USER_SSL = False
